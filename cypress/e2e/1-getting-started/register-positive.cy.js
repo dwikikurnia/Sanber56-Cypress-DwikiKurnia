0eb6772de295c6969862153df6d480d10907ccc2
Dwiki Kurnia <dwiki_kurnia@telkomsel.co.id>
2024-05-18 07:54:47 +0700

    Update random number to email for register pass

--- cypress/e2e/1-getting-started/register-positive.cy.js
+++ cypress/e2e/1-getting-started/register-positive.cy.js
@@ -6,10 +6,13 @@ describe("Registration Feature", () => {
   });
 
   it("Register Positive Testing", () => {
+    const randomNum = Math.floor(Math.random() * 10000); // Generate a random number between 0 and 9999
+    const email = `dwiki.kurnia${randomNum}@yahoo.com`;
+
     cy.get("#gender-male").check();
     cy.get("#FirstName").type("Dwiki");
     cy.get("#LastName").type("Kurnia");
-    cy.get("#Email").type("dwiki.kurnia2@yahoo.com");
+    cy.get("#Email").type(email);
     cy.get("#Password").type("qualityassurance123");
     cy.get("#ConfirmPassword").type("qualityassurance123");
     cy.get("#register-button").click();
