19c129fae14d588cdc991e9523ae26314612df87
Dwiki Kurnia <dwiki_kurnia@telkomsel.co.id>
2024-05-18 09:13:48 +0700

    Fix Edit Address New

--- cypress/e2e/1-getting-started/edit-address-positive.cy.js
+++ cypress/e2e/1-getting-started/edit-address-positive.cy.js
@@ -5,18 +5,17 @@ describe("Edit Address", () => {
     cy.visit("/");
   });
 
-  it("Edit address", () => {
+  it("Edit Address", () => {
     cy.login(user.email, user.password);
-    cy.visit("/customer/address/");
+    cy.visit("/customer/address/new/");
+    cy.get("#telephone").should("be.visible").type(user.address.phone);
+    cy.get("#street_1").should("be.visible").type(user.address.street);
+    cy.get("#city").should("be.visible").type(user.address.city);
+    cy.get("#region_id").should("be.visible").select(user.address.state);
+    cy.get("#zip").should("be.visible").type(user.address.zip);
     cy.get('button[title="Save Address"]', { timeout: 10000 })
       .should("be.visible")
       .click();
-    cy.get("#telephone").type(user.address.phone);
-    cy.get("#street_1").type(user.address.street);
-    cy.get("#city").type(user.address.city);
-    cy.get("#region_id").select(user.address.state);
-    cy.get("#zip").type(user.address.zip);
-    cy.get('button[title="Save Address"]').click();
     cy.get(".message-success").should("contain", "You saved the address.");
   });
 });
